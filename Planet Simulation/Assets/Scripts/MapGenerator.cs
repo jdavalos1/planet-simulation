4bc13c476f1e6237fe7c1edc420700272d4275f5
Nick <nicolae.bucsa.810@my.csun.edu>
2021-04-03 14:42:20 -0700

    heights added

--- Planet Simulation/Assets/Scripts/MapGenerator.cs
+++ Planet Simulation/Assets/Scripts/MapGenerator.cs
@@ -19,6 +19,9 @@ public class MapGenerator : MonoBehaviour
     public int seed;
     public Vector2 offset;
 
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
     public bool autoUpdate;
 
     public TerrainType[] regions;
@@ -56,7 +59,7 @@ public class MapGenerator : MonoBehaviour
         }
         else if (drawMode == DrawMode.Mesh)
         {
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
         }
     }
 
