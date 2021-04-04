61011ce6bd27404d3e44e5cd78e9a024cc8cebfa
Juan Davalos <jcharlied13@gmail.com>
2021-04-04 00:01:48 -0700

    Added some media and an audio manager
    Currently  audio plays for landing and jumping. A boost feature was added.

--- Planet Simulation/Assets/Scripts/Player Movement/PlayerMovement.cs
+++ Planet Simulation/Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -4,38 +4,97 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    public float maxSpeed = 15f;
+    public float minSpeed = 6f;
 
-    public float playerSpeed = 12f;
+    // Boost the player
+    public float acceleration = 10f;
+    public float deceleration = 10f;
+    
+    public CharacterController controller;
+    
+    // Player's stats at the beginning
+    public float playerSpeed;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
-
+    
+    // Parameters for ground checks
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    // Private stats of the 
     Vector3 velocity;
     bool isGrounded;
+    bool jumped = true;
+
+
+    void Start()
+    {
+        playerSpeed = minSpeed;
+    }
 
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // Control jump
+        HandleJump();
+        // Control lateral movement
+        HandleLateralMovement();
+    }
 
+    void HandleJump()
+    {
+        // Calculate if it's on the floor
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // If it's on the floor and should is still accelerating or has velocity
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+            // Did it previously jump
+            if (jumped)
+            {
+                jumped = false;
+                FindObjectOfType<AudioManager>().Play("Land");
+
+            }
         }
+
+        // If it's on the floor and the jump button was pressed jump w/sound
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            FindObjectOfType<AudioManager>().Play("Jump");
+            jumped = true;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+    void HandleLateralMovement()
+    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * playerSpeed * Time.deltaTime);
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            if(playerSpeed < maxSpeed) playerSpeed += acceleration * Time.deltaTime;
         }
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        else
+        {
+            if(playerSpeed > minSpeed)
+            {
+                playerSpeed -= deceleration * Time.deltaTime;
+            }
+            else
+            {
+                playerSpeed = minSpeed;
+            }
+        }
+
+        controller.Move(move * playerSpeed * Time.deltaTime);
     }
 }
