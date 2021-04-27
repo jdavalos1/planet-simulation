a03719b7cd09e980de0e4e1f4eb6cacffdc7c59e
Juan Davalos <jcharlied13@gmail.com>
2021-04-26 21:03:48 -0700

    Temp commit
    currently the program works but need to switch the movement for 3rd person

---
+++ Planet Simulation/Assets/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera[] roverCameras;
+    public Camera thirdPerson;
+
+    private AroundRover roverScript;
+    private MouseFollow cameraMouseFollowScript;
+
+    private void Start()
+    {
+        roverScript = FindObjectOfType<AroundRover>();
+        cameraMouseFollowScript = FindObjectOfType<MouseFollow>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MainCameraViewSwap();
+    }
+    
+    void MainCameraViewSwap()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (thirdPerson.enabled)
+            {
+                thirdPerson.enabled = false;
+                roverScript.enabled = false;
+                cameraMouseFollowScript.enabled = true;
+                foreach (var camera in roverCameras) camera.enabled = true;
+            }
+            else
+            {
+                thirdPerson.enabled = true;
+                roverScript.enabled = true;
+                cameraMouseFollowScript.enabled = false;
+                foreach (var camera in roverCameras) camera.enabled = false;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwapCurrentCameras(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SwapCurrentCameras(2);
+        }
+
+    }
+    void SwapCurrentCameras(int i)
+    {
+        var tempTexture = roverCameras[i].targetTexture;
+        roverCameras[i].targetTexture = roverCameras[0].targetTexture;
+        roverCameras[0].targetTexture = tempTexture;
+
+        var temp = roverCameras[i];
+        roverCameras[i] = roverCameras[0];
+        roverCameras[0] = temp;
+    }
+}
