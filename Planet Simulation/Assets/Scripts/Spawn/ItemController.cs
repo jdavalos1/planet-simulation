157627b57521ed94a3d6f92e07c53b8d7f980553
Juan Davalos <jcharlied13@gmail.com>
2021-05-13 14:48:38 -0700

    Changed some of the folder structure and added scripts
    The folder structure for the prefabs is fixed. The script for the basic energy cell and item spawner now exists.

---
+++ Planet Simulation/Assets/Scripts/Spawn/ItemController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Controls the spawn of the items
+public class ItemController : MonoBehaviour
+{
+    public Vector3 maxPositions;
+    public GameObject itemToSpawn;
+    [Min(0)]
+    public int maxItemSpawns;
+    public float timeForNextSpawn;
+
+    private List<GameObject> itemsNearPlayer;
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemsNearPlayer = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        time += Time.deltaTime;
+
+        if(time >= timeForNextSpawn)
+        {
+            CreateSpawn();
+        }
+
+        // Remove all items that no longer exist
+        RemoveEmpty();
+    }
+
+    void CreateSpawn()
+    {
+        time = 0;
+        if (itemsNearPlayer.Count >= maxItemSpawns) return;
+    }
+    
+    void RemoveEmpty()
+    {
+        itemsNearPlayer.RemoveAll(item => item == null);
+    }
+}
---
+++ Planet Simulation/Assets/Scripts/Spawn/Items/ItemConfigs.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemConfigs : MonoBehaviour
+{
+    private readonly float maxEuclidDist = 50f;
+    private readonly float bobSpeed = 1f;
+    private readonly float bobHeight = 0.25f;
+    private float originalY;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalY = transform.position.y;
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BobItem();
+        if (CheckItemOutOfBounds())
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void BobItem()
+    {
+        transform.position = new Vector3(transform.position.x, 
+                                originalY + Mathf.Sin(Time.time * bobSpeed) * bobHeight,
+                                transform.position.z);
+    }
+
+    bool CheckItemOutOfBounds()
+    {
+        return Vector3.Distance(player.position, transform.position) > maxEuclidDist;
+    }
+}
