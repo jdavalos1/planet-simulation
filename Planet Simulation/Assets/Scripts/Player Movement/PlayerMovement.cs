689e0e5ce8e26280e440bafe47cdc3112fdf2dfd
Juan Davalos <jcharlied13@gmail.com>
2021-05-16 13:05:26 -0700

    Linear increase on boost added

--- Planet Simulation/Assets/Scripts/Player Movement/PlayerMovement.cs
+++ Planet Simulation/Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -7,28 +7,34 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+    // Rover attributes
     public float playerSpeed;
     public float gravity;
     public float jumpHeight;
     public float maxBoostVelocity;
-    public float maxBoostTime;
+    public float boostRatio;
+    private Vector3 velocity;
+    private float currentBoost;
+    private bool boostLock;
 
+    // Hitbox attributes
     public Transform groundCheck;
     public LayerMask groundMask;
     public float groundDistance;
-
-    private EnergyBar energyBar;
-    private Vector3 velocity;
-    private AudioManager sfxManager;
-    private float boostVelocity = 1.0f;
     private bool isGrounded;
-    private bool boostLock;
+    
+    // External attributes
+    private EnergyBar energyBar;
+    private AudioManager sfxManager;
 
     void Start()
     {
+        // External params
         energyBar = gameObject.GetComponent<EnergyBar>();
         sfxManager = GameObject.Find("SFX Manager").GetComponent<AudioManager>();
+        // Necessary params
         boostLock = false;
+        currentBoost = 1.0f;
     }
 
     void Update()
@@ -36,13 +42,21 @@ public class PlayerMovement : MonoBehaviour
         CheckBoost();
         LateralMovement();
     }
-
+    
+    // Accelerate the vehicle
     void CheckBoost()
     {
+        // Boost if needed
         if (Input.GetButton("Boost"))
         {
-            boostVelocity = maxBoostVelocity;
+            // increase per frame while the boost button is on
             energyBar.DecreaseOnBoost();
+            if(currentBoost < maxBoostVelocity)
+            {
+                currentBoost += boostRatio * Time.deltaTime;
+                currentBoost = Mathf.Clamp(currentBoost, 1.0f, maxBoostVelocity);
+            }
+
             if (!boostLock)
             {
                 sfxManager.Play("Boost");
@@ -51,11 +65,17 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            boostVelocity = 1.0f;
+            // Have we reached min boost velocity? If not decrease
+            if (currentBoost > 1.0f)
+            {
+                currentBoost -= boostRatio * Time.deltaTime;
+                currentBoost = Mathf.Clamp(currentBoost, 1.0f, maxBoostVelocity);
+            }
             boostLock = false;
         }
     }
 
+    // Lateral movement in all axis (x y z)
     void LateralMovement()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -70,7 +90,7 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * playerSpeed * boostVelocity * Time.deltaTime);
+        controller.Move(move * playerSpeed * currentBoost * Time.deltaTime);
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             sfxManager.Play("Jump");
